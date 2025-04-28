bfe3530050cf1b21b14f5a10fd622a889cbeea0a
Carlos Notebook <carlosalfredoisensee@gmail.com>
2025-04-28 08:47:50 -0300

    arrumando a q05

--- src/ExArrayQuestao05.java
+++ src/ExArrayQuestao05.java
@@ -5,19 +5,20 @@ public class ExArrayQuestao05 {
         Scanner scan = new Scanner(System.in);
 
         int[] nums = new int[10];
-        int soma = 0;
+        float soma = 0;
 
         for (int i = 0; i < 10; i++) {
             System.out.print("Valor " + (i+1) + ": ");
             nums[i] = scan.nextInt();
             soma += nums[i];
         }
-        int media = soma/nums.length;
+        float media = soma/nums.length;
         System.out.println("A média é " + media);
         for (int i = 0; i < 10; i++) {
             if (nums[i] > media) {
-                System.out.println(nums[i] + " Esta acima da média, com o indice " + i);
+                System.out.println(nums[i] + " Esta acima da média, com o indice " + (i + 1));
             }
         }
+        scan.close();
     }
 }
